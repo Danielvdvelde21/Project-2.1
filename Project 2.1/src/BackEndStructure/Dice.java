74455d56eb148cf904bc9e67aec60a346ef630f7
Mc1cheeseburger <43991581+Mc1cheeseburger@users.noreply.github.com>
2020-09-23 15:34:58 +0200

    Dicing in one go
    Running all the way to the end of combat works

--- Project 2.1/src/BackEndStructure/Dice.java
+++ Project 2.1/src/BackEndStructure/Dice.java
@@ -1,6 +1,11 @@
 package BackEndStructure;
 
 public class Dice {
+
+    public static void main(String[] args) {
+        combat(10, 15);
+    }
+
     // Number of eyes rolled
     private int eyes;
 
@@ -20,68 +25,87 @@ public class Dice {
     }
 
     // Rolling for combat
-    public void combat(int attackers, int defenders){
+    public static void combat(int attackers, int defenders){
         int attackerDeaths = 0;
         int defenderDeaths = 0;
-        int[] attackerDice = new int[3];
-        int[] defendersDice = new int[2];
+        while(attackers-1  > attackerDeaths && defenders > defenderDeaths) {
+            int[] attackerDice = new int[3];
+            int[] defendersDice = new int[2];
 
-        for(int i = 0; i < attackers; i++){
-            attackerDice[i] = (int)(Math.random()*6+1);
-        }
-        for (int j = 0; j < defenders; j++){
-            defendersDice[j] = (int)(Math.random()*6+1);
-        }
-
-        // listing all the dice we need to save
-        int max = attackerDice[0];
-        int ndMax = attackerDice[0];
-        int defMax = defendersDice[0];
-        int defNdMax = defendersDice[0];
-        // saving all the dice that are the highest
-        for(int i = 1; i < 3;i++) {
-            if(attackerDice[i] > max) {
-                max = attackerDice[i];
-            }
-            else if(attackerDice[i] > ndMax){
-                ndMax = attackerDice[i];
-            }
-            if(i < 2){
-                if(defendersDice[i] > defMax){
-                    defMax = defendersDice[i];
-                }
-                else if(defendersDice[i] > defNdMax){
-                    defNdMax = defendersDice[i];
+            //throw all the dice
+            int attackersLeft = attackers - attackerDeaths;
+            int defendersLeft = defenders - defenderDeaths;
+            // Attacker dice
+            if(attackersLeft > 2) {
+                for (int i = 0; i < 3; i++) {
+                    attackerDice[i] = (int) (Math.random() * 6 + 1);
                 }
             }
-        }
-        // comparing both highest to eachother and updating each time.
-        if(defenders == 1 || attackers == 1) {
-            if(max > defMax){
-                System.out.println("Defenders lose 1 troop");
-                defenderDeaths++;
+            else{
+                for (int i = 0; i < attackersLeft; i++) {
+                    attackerDice[i] = (int) (Math.random() * 6 + 1);
+                }
+            }
+            // Defender dice
+            if(defendersLeft > 1) {
+                defendersDice[0] = (int) (Math.random() * 6 + 1);
+                defendersDice[1] = (int) (Math.random() * 6 + 1);
             }
             else{
-                System.out.println("Attackers lose 1 troop");
-                attackerDeaths++;
-            }
-        }
-        else{
-            if(max > defMax && ndMax > defNdMax){
-                System.out.println("Defenders lose 2 troops!");
-                defenderDeaths += 2;
-            }
-            else if(defMax > max && defNdMax > ndMax){
-                System.out.println("Attackers lose 2 troops!");
-                attackerDeaths += 2;
-            }
-            else{
-                System.out.println("Attackers and Defenders both lose 1 troop");
-                attackerDeaths++;
-                defenderDeaths++;
+                defendersDice[0] = (int) (Math.random() * 6 + 1);
             }
 
-        }
+            // listing all the dice we need to save
+            int max = 0;
+            int ndMax = 0;
+            int defMax = 0;
+            int defNdMax = 0;
+            // saving all the dice that are the highest
+            for (int i = 0; i < 3; i++) {
+                if (attackerDice[i] > max) {
+                    max = attackerDice[i];
+                } else if (attackerDice[i] > ndMax) {
+                    ndMax = attackerDice[i];
+                }
+                if (i < 2) {
+                    if (defendersDice[i] > defMax) {
+                        defMax = defendersDice[i];
+                    } else if (defendersDice[i] > defNdMax) {
+                        defNdMax = defendersDice[i];
+                    }
+                }
+            }
+
+            // comparing both highest to eachother and updating each time.
+            System.out.println("attackersleft " + attackersLeft);
+            System.out.println("defendersleft " + defendersLeft);
+
+            if (defendersLeft == 1 || attackersLeft == 2) {
+                if (max > defMax) {
+                    System.out.println("Defenders lose 1 troop");
+                    defenderDeaths++;
+                } else {
+                    System.out.println("Attackers lose 1 troop");
+                    attackerDeaths++;
+                }
+            } else {
+                if (max > defMax && ndMax > defNdMax) {
+                    System.out.println("Defenders lose 2 troops!");
+                    defenderDeaths += 2;
+                } else if (defMax >= max && defNdMax >= ndMax) {
+                    System.out.println("Attackers lose 2 troops!");
+                    attackerDeaths += 2;
+                } else {
+                    System.out.println("Attackers and Defenders both lose 1 troop");
+                    attackerDeaths++;
+                    defenderDeaths++;
+                }
+            }
+        }  // while loop ends here
+        int attackersAtEnd = attackers - attackerDeaths;
+        int defendersAtEnd = defenders - defenderDeaths;
+        System.out.println("The attacker has " + attackersAtEnd + " troops left!");
+        System.out.println("The defender has " + defendersAtEnd + " troops left!");
     }
 
     // Determines who can start placing infantry
