f86889fecc77a80574bc28d11465c1c638ef498f
Danielvdvelde21 <56159092+Danielvdvelde21@users.noreply.github.com>
2020-09-23 13:54:35 +0200

    territory bug fix

--- Project 2.1/src/BackEndStructure/Graph/GraphCreationMap.java
+++ Project 2.1/src/BackEndStructure/Graph/GraphCreationMap.java
@@ -5,57 +5,57 @@ public class GraphCreationMap {
 
         // NA
         Territory t0 = new Territory("Alaska");
-        Territory t0 = new Territory("Alberta");
-        Territory t0 = new Territory("Central America");
-        Territory t0 = new Territory("Eastern United States");
-        Territory t0 = new Territory("Greenland");
-        Territory t0 = new Territory("Northwest Territory");
-        Territory t0 = new Territory("Ontario");
-        Territory t0 = new Territory("Quebec");
-        Territory t0 = new Territory("Western United States");
+        Territory t1 = new Territory("Alberta");
+        Territory t2 = new Territory("Central America");
+        Territory t3 = new Territory("Eastern United States");
+        Territory t4 = new Territory("Greenland");
+        Territory t5 = new Territory("Northwest Territory");
+        Territory t6 = new Territory("Ontario");
+        Territory t7 = new Territory("Quebec");
+        Territory t8 = new Territory("Western United States");
 
         // SA
-        Territory t0 = new Territory("Argentina");
-        Territory t0 = new Territory("Brazil");
-        Territory t0 = new Territory("Peru");
-        Territory t0 = new Territory("Venezuela");
+        Territory t9 = new Territory("Argentina");
+        Territory t10 = new Territory("Brazil");
+        Territory t11 = new Territory("Peru");
+        Territory t12 = new Territory("Venezuela");
 
         // Europe
-        Territory t0 = new Territory("Great Britain");
-        Territory t0 = new Territory("Iceland");
-        Territory t0 = new Territory("Northern Europe");
-        Territory t0 = new Territory("Scandinavia");
-        Territory t0 = new Territory("Southern Europe");
-        Territory t0 = new Territory("Ukraine");
-        Territory t0 = new Territory("Western Europe");
+        Territory t13 = new Territory("Great Britain");
+        Territory t14 = new Territory("Iceland");
+        Territory t15 = new Territory("Northern Europe");
+        Territory t16 = new Territory("Scandinavia");
+        Territory t17 = new Territory("Southern Europe");
+        Territory t18 = new Territory("Ukraine");
+        Territory t19 = new Territory("Western Europe");
 
         // Africa
-        Territory t0 = new Territory("Congo");
-        Territory t0 = new Territory("East Africa");
-        Territory t0 = new Territory("Egypt");
-        Territory t0 = new Territory("Madagascar");
-        Territory t0 = new Territory("North Africa");
-        Territory t0 = new Territory("South Africa");
+        Territory t20 = new Territory("Congo");
+        Territory t21 = new Territory("East Africa");
+        Territory t22 = new Territory("Egypt");
+        Territory t23 = new Territory("Madagascar");
+        Territory t24 = new Territory("North Africa");
+        Territory t25 = new Territory("South Africa");
 
         // Asia
-        Territory t0 = new Territory("Afghanistan");
-        Territory t0 = new Territory("China");
-        Territory t0 = new Territory("India");
-        Territory t0 = new Territory("Irkutsk");
-        Territory t0 = new Territory("Japan");
-        Territory t0 = new Territory("Kamchatka");
-        Territory t0 = new Territory("Middle East");
-        Territory t0 = new Territory("Mongolia");
-        Territory t0 = new Territory("Siam");
-        Territory t0 = new Territory("Siberia");
-        Territory t0 = new Territory("Ural");
-        Territory t0 = new Territory("Yakutsk");
+        Territory t26 = new Territory("Afghanistan");
+        Territory t27 = new Territory("China");
+        Territory t28 = new Territory("India");
+        Territory t29 = new Territory("Irkutsk");
+        Territory t30 = new Territory("Japan");
+        Territory t31 = new Territory("Kamchatka");
+        Territory t32 = new Territory("Middle East");
+        Territory t33 = new Territory("Mongolia");
+        Territory t34 = new Territory("Siam");
+        Territory t35 = new Territory("Siberia");
+        Territory t36 = new Territory("Ural");
+        Territory t37 = new Territory("Yakutsk");
 
         // Australia
-        Territory t0 = new Territory("Eastern Australia");
-        Territory t0 = new Territory("Indonesia");
-        Territory t0 = new Territory("New Guinea");
-        Territory t0 = new Territory("Western Australia");
+        Territory t38 = new Territory("Eastern Australia");
+        Territory t39 = new Territory("Indonesia");
+        Territory t40 = new Territory("New Guinea");
+        Territory t41 = new Territory("Western Australia");
 
     }
 }
