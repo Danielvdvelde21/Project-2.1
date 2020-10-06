823907b99776e0b82ffeaeefe1f1ea9c821501be
Danielvdvelde21 <56159092+Danielvdvelde21@users.noreply.github.com>
2020-10-06 15:29:48 +0200

    adjecent vertices

--- Project 2.1/src/BackEndStructure/Graph/Graph.java
+++ Project 2.1/src/BackEndStructure/Graph/Graph.java
@@ -16,6 +16,15 @@ public class Graph {
         vertex2.getEdges().add(new Edge(vertex1));
     }
 
+    public boolean isAdjecent(Vertex vertex1, Vertex vertex2) {
+        for (int i = 0; i < vertex1.getEdges().size(); i++) {
+            if(vertex1.getEdges().get(i).getVertex() == vertex2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public void printGraph(){
         for (Vertex v : territoryGraph){
             System.out.print("Territory name: "+ v.getTerritory().getTerritoryName());
