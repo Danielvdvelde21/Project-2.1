34102f78792cefdea3c9f2b4aa6f4e1252105468
Danielvdvelde <danielvdvelde21@gmail.com>
2020-11-18 12:59:16 +0100

    Sexy card logic

--- Project 2.1/src/AI/AiTemplate.java
+++ Project 2.1/src/AI/AiTemplate.java
@@ -2,11 +2,15 @@ package AI;
 
 import BackEndStructure.Entities.Cards.Card;
 import BackEndStructure.Entities.Player;
+import BackEndStructure.Game.Game;
 import BackEndStructure.Graph.Edge;
 import BackEndStructure.Graph.Graph;
+import BackEndStructure.Graph.Territory;
 import BackEndStructure.Graph.Vertex;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class AiTemplate {
@@ -166,8 +170,77 @@ public class AiTemplate {
      * @return a list of cards that the bot wants to turn in
      */
     public ArrayList<Card> cards(Graph g, Player p) {
-        // TODO
-        return null;
+        // No sets available if the bot has less than 3 cards
+        if (p.getHand().size() < 3) {
+            return null;
+        }
+
+        // Get all territories owned
+        ArrayList<Territory> territoriesOwned = getOwnedTerritories(g, p);
+        int h = p.getHand().size();
+
+        // Evaluate what territories owned match with the cards
+        ArrayList<Card> preferredCards = new ArrayList<>();
+        for (Card c : p.getHand()) {
+            for (Territory t : territoriesOwned) {
+                if (c.getCardName().equals(t.getTerritoryName())) {
+                    preferredCards.add(c);
+                }
+            }
+        }
+
+        // Create each possible set of cards
+        ArrayList<Card> bestSet = new ArrayList<>();
+        int maxPrefSetsUsed = 0;
+
+        for (int i = 1; i < h; i++) {
+            for (int j = i + 1; j < h; j++) {
+                for (int k = j + 1; k < h; k++) {
+                    ArrayList<Card> tempSet = new ArrayList<>(Arrays.asList(p.getHand().get(i), p.getHand().get(j), p.getHand().get(k)));
+                    // Check if the tempSet is a valid set
+                    if (isSet(tempSet)) {
+                        // Check how many preferredCards the set is using
+                        int prefCardsUsed = 0;
+                        for (Card c : tempSet) {
+                            for (Card prefCard : preferredCards) {
+                                if (c == prefCard) {
+                                    prefCardsUsed++;
+                                }
+                            }
+                        }
+                        if (prefCardsUsed > maxPrefSetsUsed) {
+                            bestSet = tempSet;
+                            maxPrefSetsUsed = prefCardsUsed;
+                        }
+                    }
+                }
+            }
+        }
+        return bestSet;
+    }
+
+    // Sets are always pairs of 3 cards
+    private boolean isSet(ArrayList<Card> cards) {
+        int countInfantry = 0;
+        int countCavalry = 0;
+        int countArtillery = 0;
+
+        for (Card c : cards) {
+            String cardType = c.getCardType();
+            switch (cardType) {
+                case "WILDCARD":
+                    return true;
+                case "Infantry":
+                    countInfantry++;
+                    break;
+                case "Cavalry":
+                    countCavalry++;
+                    break;
+                case "Artillery":
+                    countArtillery++;
+            }
+        }
+        return countInfantry == 3 || countCavalry == 3 || countArtillery == 3 || (countInfantry == 1 && countCavalry == 1 && countArtillery == 1);
     }
 
     // -----------------------------------------------------------------------------------------------------------------
@@ -271,7 +344,7 @@ public class AiTemplate {
 
     // For a continent, calculate what percentage is owned by a player
     private double percentageOfContinentOwned(Graph g, Player p, String continent) {
-        int [] territories = continentDetector(continent);
+        int[] territories = continentDetector(continent);
 
         int counter = 0;
         for (int territory : territories) {
@@ -285,7 +358,7 @@ public class AiTemplate {
 
     // For a continent get the number of troops on it
     private int getTroopsOnContinent(Graph g, String continent) {
-        int [] territories = continentDetector(continent);
+        int[] territories = continentDetector(continent);
 
         int totalTroops = 0;
         for (int t : territories) {
@@ -296,7 +369,7 @@ public class AiTemplate {
 
     // For a continent get the number of troops on it for a given player
     private int getTroopsOnContinent(Graph g, Player p, String continent) {
-        int [] territories = continentDetector(continent);
+        int[] territories = continentDetector(continent);
 
         int totalTroops = 0;
         for (int t : territories) {
@@ -305,6 +378,19 @@ public class AiTemplate {
             }
         }
         return totalTroops;
+    }
+
+    // Get all owned territories for a player
+    private ArrayList<Territory> getOwnedTerritories(Graph g, Player p) {
+        ArrayList<Territory> territoriesOwned = new ArrayList<>();
+
+        for (int i = 0; i < g.getSize(); i++) {
+            if (g.get(i).getTerritory().getOwner().equals(p.getName())) {
+                territoriesOwned.add(g.get(i).getTerritory());
+            }
+        }
+
+        return territoriesOwned;
     }
 
 }
