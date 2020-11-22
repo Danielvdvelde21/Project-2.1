f53220b15321ff05b9f7e72bf02b7272a0d34814
joepm95 <jpamuijrers@gmail.com>
2020-11-22 16:27:23 +0100

    Attacking WIP

--- Project 2.1/src/AI/BasicBot/Components/BotAttacking.java
+++ Project 2.1/src/AI/BasicBot/Components/BotAttacking.java
@@ -25,25 +25,86 @@ public class BotAttacking extends UsefulMethods {
      * @param p This is the current player turn
      * @return A vertex array with position 0 attacker and position 1 defender
      */
-    public Vertex[] attackNew(Graph g, Player p) {
+    public Vertex[] attack(Graph g, Player p) {
         // Idea: Give countries a grade based on how likely you want to attack them/which country you want to use to attack
         // Negative scores for enemy countries, positive scores for owned countries
         double[] countries = new double[g.getSize()];
         int atk = 0;
         int def = 0;
 
+        //
+        // Troop count
+        // Negative score
         for (int i = 0; i < countries.length; i++) {
-            if (g.get(i).getTerritory().getOwner() == p) {
-                countries[i] = g.get(i).getTerritory().getNumberOfTroops();
-                atk = i;
-            }
-            else {
-                countries[i] = - g.get(i).getTerritory().getNumberOfTroops();
-                def = i;
+            if (g.get(i).getTerritory().getOwner() != p) {
+                countries[i] -= g.get(i).getTerritory().getNumberOfTroops() / 10.0;
             }
         }
-        return new Vertex[]{g.get(atk), g.get(def)};
+
+        // BSR
+        // Border Security Risk = troops in surrounding enemy territories / troops on territory itself
+        /*double[] bsr = new double[g.getSize()];
+        for (int i = 0; i < g.getSize(); i++) {
+            // Only calculate BSR for bot-owned territories
+            if (g.get(i).getTerritory().getOwner() == p) {
+                bsr[i] = g.get(i).getBSR();
+            } else {
+                bsr[i] = -1;
+            }
+        }*/
+
+        // Continent
+        // Extra continent = extra troops
+        String cont = mostOwnedContinent(g, p);
+        System.out.println(cont);
+        int[] countriesInCont = continentDetector(cont);
+        for (int i = 0; i < countriesInCont.length; i++) {
+            // Check which countries are not yet owned by the bot in the continent
+            if (g.get(countriesInCont[i]).getTerritory().getOwner() != p) {
+                countries[countriesInCont[i]] += 5.0;
+            }
+        }
+
+
+        // Cards
+        // When turning a set in, get (max) 2 extra armies if you own one of the territories on the card
+        // low weight
+        ArrayList<Card> cards = p.getHand();
+        if (p.getHand() != null) {
+            for (int i = 0; i < cards.size(); i++) {
+                for (int j = 0; j < g.getSize(); j++) {
+                    // Check if an unowned territory is on one of the bot's cards
+                    if (g.get(j).getTerritory().getTerritoryName().equals(cards.get(i).getCardName()) && g.get(j).getTerritory().getOwner() != p) {
+                        countries[i] += 1.0;
+                    }
+                }
+            }
+        }
+
+        double maxDef = countries[0];
+        int maxDefIndex = 0;
+        for (int i = 1; i < countries.length; i++) {
+            if (countries[i] > maxDef) {
+                maxDef = countries[i];
+                maxDefIndex = i;
+            }
+        }
+        LinkedList<Edge> edges = g.get(maxDefIndex).getEdges();
+        int maxAtk = 0;
+        int maxAtkIndex = 0;
+        for(int i = 0; i < edges.size(); i++) {
+            if (edges.get(i).getVertex().getTerritory().getOwner() == p) {
+                if (edges.get(i).getVertex().getTerritory().getNumberOfTroops() > maxAtk) {
+                    maxAtk = edges.get(i).getVertex().getTerritory().getNumberOfTroops();
+                    maxAtkIndex = i;
+                }
+            }
+        }
+        getAttackerDie();
+
+        return new Vertex[]{edges.get(maxAtkIndex).getVertex(), g.get(maxDefIndex)};
     }
+
 
 
     // Evaluate when bot stops attacking
@@ -71,6 +132,7 @@ public class BotAttacking extends UsefulMethods {
     }
 
     public int getAttackerDie() {
+        attackerDie = 3;
         return attackerDie;
     }
 }
